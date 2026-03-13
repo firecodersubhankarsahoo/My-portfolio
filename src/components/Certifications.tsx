5915d36d6ecfae69ed6cc81929d8546294a51f3f
subhankar <sahoosubhankar98325@gmail.com>
2026-03-13 14:26:07 +0530

    cer

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -4,52 +4,52 @@ const certificationsData = [
   {
     title: "Software Engineering",
     issuer: "IIT Kharagpur",
-    link: "",
+    link: "https://drive.google.com/file/d/1C5qROWuGg04e0tiz6XxE7uhZ1pIoxz90/view?usp=sharing",
   },
   {
     title: "Organizational Behaviour",
     issuer: "IIT Guwahati",
-    link: "",
+    link: "https://drive.google.com/file/d/1sk214Q1ErhxWopk0SwuahFNrgb-uxOFk/view",
   },
   {
     title: "Leadership and Team Effectiveness",
     issuer: "IIT Roorkee",
-    link: "",
+    link: "https://drive.google.com/file/d/1OAL_uzz0q97MdDxcbsq7gTH-DLnpkPtj/view?usp=sharing",
   },
   {
     title: "Project-Based Learning",
     issuer: "GLA University",
-    link: "",
+    link: "https://drive.google.com/file/d/1NVRpE3888Co0zmXy9S4nWF52zhLkFDe5/view",
   },
   {
     title: "JavaScript",
     issuer: "Infosys Springboard",
-    link: "",
+    link: "https://drive.google.com/file/d/1XODfA7PpvjLYC4qKGLwiAL4l3shLbKfJ/view",
   },
   {
     title: "E-Business",
     issuer: "IIT Kharagpur",
-    link: "",
+    link: "https://drive.google.com/file/d/1EHcM3HZKLd8YE-VuSmTW7SGYYhJkheq-/view",
   },
   {
     title: "Management Information System",
     issuer: "IIT Kharagpur",
-    link: "",
+    link: "https://drive.google.com/file/d/1K6aFgHk8HdsYVHf8r_gOk5r_SD-5j3N9/view",
   },
   {
     title: "Generative AI Professional",
     issuer: "Oracle",
-    link: "",
+    link: "https://drive.google.com/file/d/1H4uj_x9OmxhAcGeATZAc0RAOR-qpfwTQ/view",
   },
   {
     title: "Data Science Professional",
     issuer: "Oracle",
-    link: "",
+    link: "https://drive.google.com/file/d/1HewmCnQQNVCZiFQcxgo8DyhR5Q9Yhp99/view",
   },
   {
     title: "AI Foundation Associate",
     issuer: "Oracle",
-    link: "",
+    link: "https://drive.google.com/file/d/1QF2Sdqcef6Xo2TnpHUbxh6zf3m55bY81/view",
   },
 ];
 
@@ -60,7 +60,7 @@ const Certifications = () => {
         <h2>
           My <span>Certifications</span>
         </h2>
-        
+
         <div className="cert-grid">
           {certificationsData.map((cert, index) => (
             <div className="cert-card" key={index}>
